7a39494e401363a9bfc56873c398189fb3691878
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-01-25 20:28:58 +0600

    Delete spaces

--- src/main/java/kg/geektech/Person.java
+++ src/main/java/kg/geektech/Person.java
@@ -1,18 +1,17 @@
 package kg.geektech;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Person {
 
     static final Logger logger = LogManager.getLogger(Person.class);
-
     public Person() {
         logger.debug("Degug message");
         logger.info("Info message");
         logger.warn("Warn message");
         logger.error("Error message");
+        logger.error("Error message 2");
         logger.fatal("Fatal message");
 
     }
